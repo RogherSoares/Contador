621b6c350b708bbab0d1ca2719936441918e00b6
Rogher Adriano Soares <163779899+Roogher@users.noreply.github.com>
2026-03-13 14:06:20 -0300

    Contador Finalizado

--- ADS/5Semestre/DevMobile/Contador/app/src/main/java/com/roghersoares/conatdor/MainActivity.java
+++ ADS/5Semestre/DevMobile/Contador/app/src/main/java/com/roghersoares/conatdor/MainActivity.java
@@ -1,4 +1,4 @@
-package com.roghersoares.contador;
+package com.roghersoares.conatdor;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -11,11 +11,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
 public class MainActivity extends AppCompatActivity {
 
-    //Define as variáveis globais (objetos de tela)
+    //Define as variaveis globais (objetos de tela)
     private int contador = 0;
+
     private TextView tvContador;
 
     @Override
@@ -23,37 +23,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Vincula os componetes XML com o Java
+        //Vincular os componentes XML com Java
         tvContador = findViewById(R.id.tvContador);
-        Button btninc = findViewById(R.id.btnIncrementar);
-        Button btndec = findViewById(R.id.btnDecrementar);
+        Button btnInc = findViewById(R.id.btnIncrementar);
+        Button btnDec = findViewById(R.id.btnDecrementar);
 
         //Evento de click para incrementar
-        btninc.setOnClickListener(v -> {
-            contador ++;
+        btnInc.setOnClickListener(v-> {
+            contador++;
             atualizarInterface();
         });
 
         //Evento de click para decrementar
-        btndec.setOnClickListener(v -> {
-            if(contador > 0) {
-                contador --;
+        btnDec.setOnClickListener(v ->{
+            if (contador >0){
+                contador--;
                 atualizarInterface();
             }
         });
     }
+
     //Método para centralizar e atualizar a UI
-    private void atualizarInterface() {
+    private void atualizarInterface(){
         tvContador.setText(String.valueOf(contador));
 
-        //Reagra de mudança de cor ao atingir limite de 10
-        if(contador >= 20) {
+        //Regra de mudança de cor ao atingir limite de 10
+        if (contador >= 20){
             tvContador.setTextColor(Color.GREEN);
-        } else if(contador >= 10) {
+        } else if (contador >=10){
             tvContador.setTextColor(Color.RED);
-        }else {
+        } else {
             tvContador.setTextColor(Color.BLACK);
         }
     }
-
 }
